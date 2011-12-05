5d47b83de26e3825c5510f25ac517d5e1c13c7aa
özgür <hinoglu@gmail.com>
2011-12-05 13:35:17 +0200

    slider falan ekledim bir kısım da düzeltme yaptım

--- spread.js
+++ spread.js
@@ -1,13 +1,17 @@
-var doc_body  = $(document.body)? $(document.body):$$('body')[0];
+/*
+ * Spread: Speed Reading Extension
+ */
+
+var doc_body  = $("body");
+jQuery.fn.exists = function(){return this.length>0;}
 
 function format(){
-    
+
     var formatted_str = arguments[0] || '';
     for(var i=1; i<arguments.length; i++){
         var re = new RegExp("\\{"+(i-1)+"}", "gim");
         formatted_str = formatted_str.replace(re, arguments[i]);
     }
-
     return formatted_str;
 }
 
@@ -106,9 +110,9 @@ var SPREAD_TEXT_READER = function(content, conf){
     this.updateSettings = function(){
         settings = {
             /* frame per minute */
-           'fpm': this.conf.fpm, 
-           'words': parseInt(this.step),
-           'font': parseInt($('spread_reader_body').getStyle('font-size'))
+            'fpm': this.conf.fpm, 
+            'words': parseInt(this.step),
+            'font': parseInt($('spread_reader_body').getStyle('font-size'))
         };
 
         this.displaySettings();
@@ -118,7 +122,7 @@ var SPREAD_TEXT_READER = function(content, conf){
     this.displaySettings = function(){
         $('infopane').textContent = format('{0} wpm / {1} words / {2}px fonts', 
                 this.conf.fpm*this.step, this.step, parseInt($('spread_reader_body').getStyle('font-size')));
-    
+
     };
 
     this.update_interval();
@@ -130,136 +134,130 @@ var SPREAD_TEXT_READER = function(content, conf){
 function create_reader(text){
     text = text.replace(/(\w+)([,\.:;\(\)\[\]\"]+)(\w+)/gim, '$1$2 $3');
     var count = text.match(/([^\s\t]+)/gim).length;
-    
+
     chrome.extension.sendRequest({command:'getSettings'}, 
             function(response){
                 var settings = response.settings; 
-                var wsize = window.getSize();
-                
                 var reader = SPREAD_TEXT_READER(text, settings);
-                var background = new Element('div', {
-                    id: 'spread_reader_background',
-                    style: format("width:{0}px;height:{1}px;", wsize.x, wsize.y), 
-                    events: {
-                        mousedown: function(){
-                            $('spread_reader').destroy();
-                            this.destroy();
-                            reader.stop();
-                            }
-                        }
-                    }
-                );
-                var div = new Element('div', {
-                    id: "spread_reader"
-                });
-                var reader_inner_container = Element('div', {
-                    id: "spread_reader_body_container"
-                });
-                var reader_inner = Element('div', {
-                    id: "spread_reader_body"
-                });
+                var w_width = $(window).width();
+                var w_height = $(window).height()
 
-                var infopane = Element ('div', {
-                    id: "infopane"
-                });
+        var background = $('<div/>', {
+            id: 'spread_reader_background',
+            style: format("width:{0}px;height:{1}px;", w_width, w_height)
+        }).mousedown(function(){
+            $('#spread_reader').remove();
+            background.remove();
+            reader.stop();
+        });
 
-                var sliderBody = Element('div', {
-                    id: 'spread_reader_slider',
-                    html: "<div id='spread_reader_knob'></div><span id='speed_reader_step'></span>"
-                });
 
-                var reader_buttons = Element('ul', {
-                    html: "<li id='spread_reader_start'>start</li>"+
-                    "<li id='spread_reader_stop'>stop</li>"+
-                    "<li id='spread_reader_restart'>restart</li>" + 
-                    "<li><hr></li>" + 
-                    "<li id='spread_reader_bigger'>bigger</li>"+
-                    "<li id='spread_reader_smaller'>smaller</li>"+
-                    "<li><hr></li>" + 
-                    "<li id='spread_reader_faster'>faster</li>"+
-                    "<li id='spread_reader_slower'>slower</li>"+
-                    "<li><hr></li>" + 
-                    "<li id='spread_reader_more_words'>more words</li>" + 
-                    "<li id='spread_reader_less_words'>less words</li>",
-                    id:'spread_reader_buttons'
-                });
 
-                //div.makeResizable();
-                reader_inner_container.grab(infopane);
-                reader_inner_container.grab(reader_inner);
-                div.grab(reader_inner_container);
-                div.grab(reader_buttons);
+    var div = $('<div id="spread_reader"></div>');
+    var reader_inner_container = $('<div id="spread_reader_body_container"></div>');
+    var reader_inner = $('<div id="spread_reader_body"></div>');
+    var infopane = $('<div id="infopane"></div>');
+    var sliderBody = $('<div id="spread_reader_slider"></div>')
+        .html("<div id='spread_reader_knob'></div><span id='speed_reader_step'></span>");
 
-                doc_body.grab(background);
-                doc_body.grab(div);
-                div.grab(sliderBody);
+    var reader_buttons = $('<ul id="spread_reader_buttons"></ul>')
+        .html(
+                "<li id='spread_reader_start'>start</li>"+
+                "<li id='spread_reader_stop'>stop</li>"+
+                "<li id='spread_reader_restart'>restart</li>" + 
+                "<li><hr></li>" + 
+                "<li id='spread_reader_bigger'>bigger</li>"+
+                "<li id='spread_reader_smaller'>smaller</li>"+
+                "<li><hr></li>" + 
+                "<li id='spread_reader_faster'>faster</li>"+
+                "<li id='spread_reader_slower'>slower</li>"+
+                "<li><hr></li>" + 
+                "<li id='spread_reader_more_words'>more words</li>" + 
+                "<li id='spread_reader_less_words'>less words</li>"
+             );
 
-                var x = (wsize.x - $(div).getSize().x)/2;
-                var y = (wsize.y - $(div).getSize().y)/2;
-                div.setStyle('top', y).setStyle('left',x);
+    //div.makeResizable();
+    reader_inner_container
+        .append(infopane)
+        .append(reader_inner);
 
-                div.addEvent('resize', function(){
-                    var wsize = window.getSize();
-                    var x = (wsize.x - $(div).getSize().x)/2;
-                    var y = (wsize.y - $(div).getSize().y)/2;
-                    this.setStyle('top', y).setStyle('left',x);
-                    
-                    var ulsize = $('spread_reader_buttons').getSize();
-                    var divsize = this.getSize();
+    div.append(reader_inner_container)
+        .append(reader_buttons);
 
-                    $(reader_inner_container).setStyle('width', divsize.x  - (ulsize.x + 3) );
-                    $(reader_inner_container).setStyle('height', divsize.y );
+    doc_body.append(background).append(div);
+    div.append(sliderBody);
 
-                });
+    var x = (w_width - $(div).width())/2;
+    var y = (w_height - $(div).height())/2;
+    div.css({'top': y,'left':x});
 
-                reader.slider = new Slider($('spread_reader_slider'), $('spread_reader_knob'), {
-	                steps: count,
-                    mode: 'vertical',
-                    snap: true,
-	                onChange: function(step){
-		                $('speed_reader_step').set('text', step);
-                        reader.idx = step; 
-	                    },
-                    }).set(0);
+    div.resize( function(event){
+        var x = (w_width - $(div).width())/2;
+        var y = (w_height - $(div).height())/2;
+        $(this).css({'top': y, 'left':x});
 
-                div.fireEvent('resize');
+        reader_inner_container
+        .css({'width': $(this).width() - (reader_buttons.width() + 3)})
+        .css({'height': $(this).height()});
 
-                
-                $('spread_reader_bigger').addEvent('click', function(){
-                        var fontsize = parseInt($('spread_reader_body').getStyle('font-size'));
-                        $('spread_reader_body').setStyle('font-size', format('{0} !important', fontsize*1.1));
-                        reader.updateSettings();
-                });
-                $('spread_reader_smaller').addEvent('click', function(){
-                        var fontsize = parseInt($('spread_reader_body').getStyle('font-size'));
-                        $('spread_reader_body').setStyle('font-size', format('{} !important', fontsize*0.9));
-                        reader.updateSettings();
-                });
-                $('spread_reader_faster').addEvent('click', function(){
-                        reader.conf.fpm += 20;
-                        reader.setSpeed();
-                        
-                });
-                $('spread_reader_slower').addEvent('click', function(){
-                        reader.conf.fpm = (reader.conf.fpm > 20)? reader.conf.fpm - 20: reader.conf.fpm;
-                        reader.setSpeed();
-                });
-                $('spread_reader_restart').addEvent('click', function(){
-                        reader.restart();
-                });
-                $('spread_reader_start').addEvent('click', function(){
-                        reader.start();
-                });
-                $('spread_reader_stop').addEvent('click', function(){
-                        reader.stop();
-                });
-                $('spread_reader_more_words').addEvent('click', function(){
-                        reader.more_words();
-                });
+    });
 
-                $('spread_reader_less_words').addEvent('click', function(){
-                        reader.less_words();
-                });
+    $('#spread_reader_knob').draggable({
+        containment:'parent',
+        axis:'y',
+        drag:function(e,ui){
+
+            if(!this.par)
+            {
+
+                this.par = $(this).parent();
+                this.parHeight = this.par.height();
+                this.height = $(this).height();
+                this.color = $.trim(this.par.attr('class').replace('colorful-slider',''));
+            }
+
+            var ratio = 1-(ui.position.top+this.height)/this.parHeight;
+
+        }
+    });
+    div.trigger('resize');
+
+
+    $('spread_reader_bigger').click(function(){
+        var fontsize = parseInt($('spread_reader_body').getStyle('font-size'));
+        $('spread_reader_body').setStyle('font-size', format('{0} !important', fontsize*1.1));
+        reader.updateSettings();
+    });
+    $('spread_reader_smaller').click( function(){
+        var fontsize = parseInt($('spread_reader_body').getStyle('font-size'));
+        $('spread_reader_body').setStyle('font-size', format('{} !important', fontsize*0.9));
+        reader.updateSettings();
+    });
+    $('spread_reader_faster').click(function(){
+        reader.conf.fpm += 20;
+        reader.setSpeed();
+
+    });
+    $('spread_reader_slower').click(function(){
+        reader.conf.fpm = (reader.conf.fpm > 20)? reader.conf.fpm - 20: reader.conf.fpm;
+        reader.setSpeed();
+    });
+    $('spread_reader_restart').click(function(){
+        reader.restart();
+    });
+    $('spread_reader_start').click( function(){
+        reader.start();
+    });
+    $('spread_reader_stop').click(function(){
+        reader.stop();
+    });
+    $('spread_reader_more_words').click( function(){
+        reader.more_words();
+    });
+
+    $('spread_reader_less_words').click( function(){
+        reader.less_words();
+    });
 
 
             });
@@ -267,33 +265,29 @@ function create_reader(text){
 
 
 
-window.addEvent('mouseup',
-    function(event){
-        var notifier = $('spread_notifier');
-        if (event.target.id !== 'spread_notifier' && notifier){
-            notifier.destroy();
-        }else {
-            var text = getSelectedText();
-            if (text.length){
-                if (!notifier){
-                    var spread_notif = new Element('div',{
-                        id: 'spread_notifier',
-                        text: 'Speed read this text',
-                        events: {
-                            'click': function(event){
-                                create_reader(text);
-                                return this.destroy();
-                            }
-                        }
-                    });
-                    $(doc_body).grab(spread_notif);
-                    $(spread_notif).setStyle('left', event.page.x + 30);
-                    $(spread_notif).setStyle('top',  event.page.y - 50);
+$(window).mouseup(
+        function(event){
+            var notifier = $('#spread_notifier');
+            if (notifier.exists() && event.target.id !== 'spread_notifier'){
+                notifier.remove();
+            }else {
+                var text = getSelectedText();
+
+                if (text.length){
+
+                    if (!notifier.exists()){
+                        var spread_notif = $(format('<div id="{0}">{1}</div>',
+                                'spread_notifier',
+                                'Speed read this text')).click(function(event){
+                                    create_reader(text);
+                                    return $(this).remove();
+                                }).css({'left': event.pageX + 30, 'top':event.pageY - 50})
+                        .appendTo(doc_body);
+                    }
                 }
             }
+            return event;
         }
-        return event;
-    }
 );
 
 
